bcf377ed640b1c74814f03a4557f0777102917ec
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-07-04 05:30:14 -0500

    update axiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
     baseURL: `http://localhost:5000`
@@ -9,12 +10,33 @@ const axiosSecure = axios.create({
 
 const useAxiosSecure = () => {
 
-    const {user}=useAuth();
+    const { user, logOut }=useAuth();
+    const navigate = useNavigate();
     axiosSecure.interceptors.request.use(config => {
         config.headers.Authorization = `Bearer ${user.accessToken}`;
         return config;
 
     }, error => {
+        return Promise.reject(error);
+    }
+    )
+
+    axiosSecure.interceptors.response.use(response => {
+        return response;
+    }, error => {
+        const status = error.status;
+        if (status === 403) {
+           navigate('/forbidden');
+        }
+        else if (status === 401) {
+            logOut()
+            .then(() => {
+                navigate('/login');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
         return Promise.reject(error);
     }
     )
